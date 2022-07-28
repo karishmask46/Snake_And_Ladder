3dc8c7d173632fcb9b29e1169bfbc3376c114651
karishmask46 <karishmask.15999@gmail.com>
2022-07-28 13:13:54 +0530

    Checking The position

--- Snake_And_ladder/Snake_And_ladder/Program.cs
+++ Snake_And_ladder/Snake_And_ladder/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Snake_And_ladder;
 Console.WriteLine("Welcome to the Snake and Ladder");
-UC2_Num1to6 obj = new UC2_Num1to6();
-obj.die();
+UC3_checkOption obj = new UC3_checkOption();
+obj.CheckPosition();
